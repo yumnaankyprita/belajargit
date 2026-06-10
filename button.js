4345cc3385c4a3141ace4a1766e3e6ebd36accc5
yumnaankyprita <yumnaankyprita@gmail.com>
2026-06-10 11:32:15 +0700

    belajar git

---
+++ button.js
@@ -0,0 +1,8 @@
+const person = {
+    nama: "yumna",
+    umur: 20,
+    hobi: ["makan", "tidur"],
+    pekerjaan: [{ nama: "direktur" }, { nama: "editor" }]
+}
+
+console.log(person.pekerjaan[0].nama)
